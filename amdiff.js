f89197260f23f2c0e0229f5de0adba1469ec207c
fabiobosisio <fbosisio@gmail.com>
2022-12-07 17:32:14 -0300

    Versão estável

--- amdiff.js
+++ amdiff.js
@@ -2,8 +2,41 @@
 
 const Automerge = require('automerge') // Carrega o automerge no node
 var fs = require("fs"); // file system
+let verbose = false; // Ativa (true) e desativa (false) o modo verboso
 
 //***************************** Funções *****************************//
+
+//Função para exibir o manual:
+function help() {
+
+console.log("AMDIFF v0.1.0")
+console.log("")
+console.log("AMDIFF is a command line comparision utility for JSON-Automerge files with save file function for differences")
+console.log("")
+console.log("Usage:")
+console.log("    node amdiff.js <file_old> <file_actual> [verbose]")
+console.log("    node amdiff.js help")
+console.log("")
+console.log("Help:")
+console.log("    Displays this manual")
+console.log("")
+console.log("File_old:")
+console.log("    Filename of previous file")
+console.log("")
+console.log("File_actual:")
+console.log("    Filename of actual file")
+console.log("")
+console.log("Verbose:")
+console.log("    Enable verbose mode")
+console.log("")
+console.log("More Information:")
+console.log("")
+console.log("    https://github.com/fabiobosisio/amdiff")
+console.log("")
+console.log("    Please report bugs at <https://github.com/fabiobosisio/amdiff/blob/master/README.md>")
+console.log("")
+}
+
 // Função de leitura do arquivo Automerge-Json
 function recompose(file){
   let output;
@@ -12,7 +45,7 @@ function recompose(file){
   file = file+".am"
   // Verifica se o arquivo existe localmente
   if(!fs.existsSync(file)) {
-    if(verbose) console.log("Arquivo inexistente");
+    ('\x1b[36m%s\x1b[0m',"Arquivo inexistente");
   }
   else {
     // Carrega o arquivo
@@ -21,16 +54,44 @@ function recompose(file){
 
   }
 }
- 
-let ant = recompose(process.argv[2])
-let pos = recompose(process.argv[3])
+
+// Função para verificar se o arquivo está vazio
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
+
+// Função de exibição do JSON formatado
+function show(out){
+  console.dir(out, { depth: null} );
+  return null;
+}
+
+//***************************** Módulo Principal *****************************//
+
+if(process.argv[4] == 'verbose'){ verbose = true;} // Habilita o modo verboso
+
+if (process.argv[2] == 'help' || process.argv[2] == 'Help' || process.argv[2] == 'h' || process.argv[2] == null  || process.argv[3] == null){ // Exibe o manual
+  help();
+  return null
+}
+
+let ant = recompose(process.argv[2]) // Carrega o arquivo anterior
+if(verbose) {console.log('\x1b[36m%s\x1b[0m',"Arquivo anterior:"); show(ant);}
+
+let pos = recompose(process.argv[3]) // Carrega o arquivo atual
+if(verbose) {console.log('\x1b[36m%s\x1b[0m',"Arquivo atual"); show(pos);}
  			
-// Obtém a diferença entre o arquivo local e o conteudo consolidado da cadeia
-let filediff  = Automerge.getChanges(ant,pos);		
+let filediff  = Automerge.getChanges(ant,pos);// Obtém a diferença entre o arquivo local e o conteudo consolidado da cadeia
+if(verbose) console.log('\x1b[36m%s\x1b[0m',"Diferenca obtida com sucesso");	
 
-// Salva o arquivo local .automerge com os metadados automerge do json
-fs.writeFileSync(process.argv[2]+'-->'+process.argv[3]+".diff", JSON.stringify(filediff), {encoding: null}); 
-						
-//console.log('\x1b[36m%s\x1b[0m',`\nDiferenca entre o arquivo local e o conteudo da cadeia:`);
-//console.log(Automerge.load(filediff));
-
+if (isEmpty(filediff)){ // Se não houver diferenças sai
+  console.log('\x1b[1m\x1b[31m%s','\nNao tem diferencas entre os arquivos!','\x1b[0m\n');
+  return null
+}else{
+  fs.writeFileSync(process.argv[2]+'>'+process.argv[3]+".diff", JSON.stringify(filediff), {encoding: null}); // Salva o arquivo local .diff com a diferença entre os dois arquivos
+  if(verbose) console.log('\x1b[36m%s\x1b[0m',"Arquivo salvo com sucesso"); 
+}
